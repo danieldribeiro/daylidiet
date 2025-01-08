a42f51178f897763bdd837f887bc80a8c6bfad57
Daniel Ribeiro <daniel.dribeiro@hotmail.com>
2025-01-08 17:33:10 -0300

    Feat: create theme and implements theme provider for the application

---
+++ src/theme/index.ts
@@ -0,0 +1,31 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    GREEN_300: '#E5F0DB',
+    GREEN_500: '#CBE4B4',
+    GREEN_700: '#639336',
+
+    RED_300: '#F4E6E7',
+    RED_500: '#F3BABD',
+    RED_700: '#BF3B44',
+
+    GRAY_100: '#FAFAFA',
+    GRAY_200: '#EFF0F0',
+    GRAY_300: '#DDDEDF',
+    GRAY_400: '#B9BBBC',
+    GRAY_500: '#5C6265',
+    GRAY_600: '#333638',
+    GRAY_700: '#1B1D1E',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Nunito_400Regular',
+    BOLD: 'Nunito_700Bold'
+  },
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24
+  }
+};
